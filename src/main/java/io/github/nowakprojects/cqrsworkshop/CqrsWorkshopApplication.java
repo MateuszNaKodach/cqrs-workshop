23dde634d36ac089678fd04bc4bbb294496db403
mnowak <mateusz.nowak@intenso.pl>
2018-12-18 20:15:58 +0100

    Initial commit

---
+++ src/main/java/io/github/nowakprojects/cqrsworkshop/CqrsWorkshopApplication.java
@@ -0,0 +1,14 @@
+package io.github.nowakprojects.cqrsworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsWorkshopApplication.class, args);
+    }
+
+}
+
